7b3be13f5798c5885a4d87af0b8fdeb1dee9dca3
Tianhao Chai <cth451@gmail.com>
2018-10-21 12:05:15 -0500

    Fix Windows-isms

--- UPCV_Generator.py
+++ UPCV_Generator.py
@@ -1,3 +1,4 @@
+#!/bin/python3
 # -*- coding:utf8 -*-
 '''
 @Author: Agent DuskPiper (github.com/duskpiper)
@@ -6,16 +7,19 @@
     This script helps transfer game_log.tsv data into IITC-draw-tool link in order to display on map your UPC or UPV.
     This script is based on python3.
 '''
+
 #import codecs
 print('---This script is composed by DuskPiper---')
 print('[NOTICE] Now load game_log.tsv. Plz put file at the same path with this script.')
 input('[...]    Press ENTER to continue.')
+
 ##LOAD TSV
-with open(r'.\game_log.tsv',encoding='utf8') as tsv:
+with open(r'./game_log.tsv',encoding='utf8') as tsv:
     data=[line.strip().split('\t') for line in tsv]
 tsv.close()
 print('[DONE]   Game log loaded.')
 data=data[1:]
+
 ##PROCESS DATA, GENERATE upc,upv
 upc,upv=set(),set()
 upv_ops=('resonator deployed','hacked','mod deployed')
@@ -27,8 +31,9 @@ for row in data:
         continue
     if row[3] in upv_ops:
         upv.add((row[1], row[2]))
+
 ##WRITE
-w=open(r'.\UPC.txt','w')
+w=open(r'./UPC.txt','w')
 w.write('[')
 for la,ln in upc:
     newline='{"latLng":{"lat":'+la+',"lng":'+ln+'},"color":"#ff00ff","radius":20,"type":"circle"},'
@@ -37,7 +42,7 @@ w.write(writer[:-1])
 w.write(']')
 w.close()
 print('[DONE]   UPC.txt generated.')
-w=open(r'.\UPV.txt','w')
+w=open(r'./UPV.txt','w')
 w.write('[')
 for la,ln in upv:
     newline='{"latLng":{"lat":'+la+',"lng":'+ln+'},"color":"#ff00ff","radius":20,"type":"circle"},'
@@ -46,6 +51,7 @@ w.write(writer[:-1])
 w.write(']')
 w.close()
 print('[DONE]   UPV.txt generated.')
+
 ##FINISH
 print('[DONE]   All finished, copy all from UPC/UPV file and paste into IITC-drawtools.')
 input('[...]    Press ENTER to EXIT.')
