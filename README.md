681624d639ac58a0a28b3e81e4cc0f410643552d
Editing42 <trumandelta@gmail.com>
2020-07-24 23:34:01 +0800

    Add upv operations

--- README.md
+++ README.md
@@ -1,18 +1,30 @@
 # ProjPixian-GDPR-to-IITC
 
-[阅读中文版本](README_CHN.md)
+[Read in English](README_EN.md)
 
-converts GDPR data to IITC draw-tools link.
-## Instruction 
-1. For Win10 users, what you actually need from here is to download UPCV_Generator.exe and keep it at any folder you like. If you wish to run it on MacOS/Linux or if your Windows machine reports that the file is not executable, please download UPCV_Generator.py and run it with Python3.
+将GDPR数据转化为IITC-draw-tool链接。
 
-2. All you need from you GDPR data is "game_log.tsv", copy it and paste at the the folder with this tool, override the one with same name.
+## 更新内容
 
-3. Then run this tool and follow instructions in the console.
+2020/07/24
 
-4. UPC.txt and UPV.txt should then be generated. Copy all from any of the two and paste in IITC-DrawTools Opt-Paste Drawn Items.
+- 更新了对 UPV 操作的匹配，包含 hack, resonator deployed, resonator upgraded，同时验证操作状态为 success。
+
+- 输出新的二合一文件（UPCV.log），用紫色标记 upc，用红色标记仅 upv 的 portal，方便区分。
+
+- 懒得装 pyinstaller，只更新了 py 文件。
+
+## 指南
+
+1. 对于有VC2015的Windows用户，下载 UPCV_Generator.exe 文件，放到任何你喜欢的目录。如果想在MacOS或Linux上运行或者Windows运行报错，则请下载UPCV_Generator.py，并使用python3运行它。
+
+2. GDPR文件之中你只需要game_log.tsv文件，将其复制粘贴到本程序的同一目录下，覆盖同名文件。
+
+3. 运行本工具并按照窗口指令操作。
+
+4. UPC.log 和 UPV.log 应当已经生成了。复制粘贴任意文件里面的内容到IITC-DrawTools Opt-Paste Drawn Items。
+
+## 提示
 
 
-
-## Tips 
-1. To acquire GDPR data from Niantic, you shall email [privacy@nianticlabs.com](mailto:privacy@nianticlabs.com) stating your rightfulness of aquiring "raw data" of your own which have been collected by Niantic. Then they will reply to you SOOOON.
+1. 获取GDPR数据的方法：给Niantic发邮件到邮箱  [privacy@nianticlabs.com](mailto:privacy@nianticlabs.com) 陈述你有权获得由Niantic收集的你的"raw data"。 然后他们很快会回复你（并不会，咕咕咕）。
--- UPCV_Generator.py
+++ UPCV_Generator.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#! /usr/bin/env python3
 # -*- coding:utf8 -*-
 '''
 @Author: Agent DuskPiper (github.com/duskpiper)
@@ -7,7 +7,6 @@
     This script helps transfer game_log.tsv data into IITC-draw-tool link in order to display on map your UPC or UPV.
     This script is based on python3.
 '''
-
 #import codecs
 print('---This script is composed by DuskPiper---')
 print('[NOTICE] Now load game_log.tsv. Plz put file at the same path with this script.')
@@ -16,45 +15,52 @@ input('[...]    Press ENTER to continue.')
 ##LOAD TSV
 with open(r'./game_log.tsv',encoding='utf8') as tsv:
     data=[line.strip().split('\t') for line in tsv]
-tsv.close()
 print('[DONE]   Game log loaded.')
 data=data[1:]
 
 ##PROCESS DATA, GENERATE upc,upv
 upc,upv=set(),set()
-upv_ops=('resonator deployed','hacked','mod deployed')
-writer=''
+upv_ops=('resonator deployed', 'resonator upgraded', 'hacked friendly portal', 'hacked enemy portal', 'hacked neutral portal')
 for row in data:
     if row[3]=='captured portal':
         upc.add((row[1],row[2]))
         upv.add((row[1],row[2]))
         continue
-    if row[3] in upv_ops:
+    if row[3] in upv_ops and row[4] == 'success':
         upv.add((row[1], row[2]))
+upv_only = upv-upc
 
 ##WRITE
-w=open(r'./UPC.txt','w')
-w.write('[')
-for la,ln in upc:
-    newline='{"latLng":{"lat":'+la+',"lng":'+ln+'},"color":"#ff00ff","radius":20,"type":"circle"},'
-    writer+=newline
-w.write(writer[:-1])
-w.write(']')
-w.close()
-print('[DONE]   UPC.txt generated.')
-w=open(r'./UPV.txt','w')
-w.write('[')
-for la,ln in upv:
-    newline='{"latLng":{"lat":'+la+',"lng":'+ln+'},"color":"#ff00ff","radius":20,"type":"circle"},'
-    writer+=newline
-w.write(writer[:-1])
-w.write(']')
-w.close()
-print('[DONE]   UPV.txt generated.')
+upc_log=''
+upv_log=''
+
+with open(r'./UPC.log','w') as f:
+    f.write('[')
+    for la,ln in upc:
+        newline='{"latLng":{"lat":'+la+',"lng":'+ln+'},"color":"#ff0000","radius":20,"type":"circle"},'
+        upc_log+=newline
+    f.write(upc_log[:-1])
+    f.write(']')
+print('[DONE]   UPC.log generated.')
+
+with open(r'./UPV.log','w') as f:
+    f.write('[')
+    for la,ln in upv:
+        newline='{"latLng":{"lat":'+la+',"lng":'+ln+'},"color":"#ff0000","radius":20,"type":"circle"},'
+        upv_log+=newline
+    f.write(upv_log[:-1])
+    f.write(']')
+print('[DONE]   UPV.log generated.')
+
+with open(r'./UPCV.log','w') as f:
+    f.write('[')
+    for la,ln in upv_only:
+        newline='{"latLng":{"lat":'+la+',"lng":'+ln+'},"color":"#a24ac3","radius":20,"type":"circle"},'
+        upc_log+=newline
+    f.write(upc_log[:-1])
+    f.write(']')
+print('[DONE]   UPCV.log generated.')
 
 ##FINISH
 print('[DONE]   All finished, copy all from UPC/UPV file and paste into IITC-drawtools.')
 input('[...]    Press ENTER to EXIT.')
-
-
-
